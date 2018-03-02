0b9d4cd965797fa8fa034dd61107fea917880c42
李铭昕 <715557344@qq.com>
2018-03-02 20:42:08 +0800

    增加Travis支持

---
+++ tests/_ci/install_prereqs_7.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+#  Phalcon Framework
+#
+#  Copyright (c) 2011-present Phalcon Team (https://www.phalconphp.com)
+#
+#  This source file is subject to the New BSD License that is bundled
+#  with this package in the file LICENSE.txt.
+#
+#  If you did not receive a copy of the license and are unable to
+#  obtain it through the world-wide-web, please send an email
+#  to license@phalconphp.com so we can send you a copy immediately.
+
+printf "\n" | pecl install --force swoole &> /dev/null
